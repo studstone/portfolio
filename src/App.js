e8b9f391c5fb3527a7051f731085264352edf917
studstone <nikolas2996@gmail.com>
2022-08-03 13:17:24 +0300

    create DB

--- src/App.js
+++ src/App.js
@@ -4,64 +4,10 @@ import NavBar from "./Components/NavBar/NavBar";
 import Preview from "./Components/Preview/Preview";
 import Skills from "./Components/Skills/Skills";
 import ClobalStyle from "./Components/StyledComponents/GlobalStyle";
+import { items, skills } from "./DB/DBItems";
 
 const App = () => {
-    const items = [
-        {
-            value: 'Домашняя',
-            href: '#preview'
-        },
-        {
-            value: 'Обо мне',
-            href: '#about'
-        },
-        {
-            value: 'Мои работы',
-            href: '#work'
-        },
-        {
-            value: 'Контакты',
-            href: '#footer'
-        }
-    ];
 
-    const skills = [
-        {
-            value: 'FLEXBOX',
-            src: './image/flexbox.svg',
-            alt: 'Icon'
-        },
-        {
-            value: 'GRID',
-            src: './image/grid.svg',
-            alt: 'Icon'
-        },
-        {
-            value: 'SCSS',
-            src: './image/scss.svg',
-            alt: 'Icon'
-        },
-        {
-            value: 'REACT',
-            src: './image/react.svg',
-            alt: 'Icon'
-        },
-        {
-            value: 'WEBPACK',
-            src: './image/webpack.svg',
-            alt: 'Icon'
-        },
-        {
-            value: 'ANIMATION',
-            src: './image/animation.svg',
-            alt: 'Icon'
-        },
-        {
-            value: 'BEM',
-            src: './image/bem.svg',
-            alt: 'Icon'
-        }
-    ];
 
     const activState = useActiveState();
 
---
+++ src/DB/DBItems.js
@@ -0,0 +1,56 @@
+export const items = [
+    {
+        value: 'Домашняя',
+        href: '#preview'
+    },
+    {
+        value: 'Обо мне',
+        href: '#about'
+    },
+    {
+        value: 'Мои работы',
+        href: '#work'
+    },
+    {
+        value: 'Контакты',
+        href: '#footer'
+    }
+];
+
+export const skills = [
+    {
+        value: 'FLEXBOX',
+        src: './image/flexbox.svg',
+        alt: 'Icon'
+    },
+    {
+        value: 'GRID',
+        src: './image/grid.svg',
+        alt: 'Icon'
+    },
+    {
+        value: 'SCSS',
+        src: './image/scss.svg',
+        alt: 'Icon'
+    },
+    {
+        value: 'REACT',
+        src: './image/react.svg',
+        alt: 'Icon'
+    },
+    {
+        value: 'WEBPACK',
+        src: './image/webpack.svg',
+        alt: 'Icon'
+    },
+    {
+        value: 'ANIMATION',
+        src: './image/animation.svg',
+        alt: 'Icon'
+    },
+    {
+        value: 'BEM',
+        src: './image/bem.svg',
+        alt: 'Icon'
+    }
+];
